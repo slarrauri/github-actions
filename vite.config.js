5e119b5955a1179329e2a503505fa949c897fafb
slarrauri <slarrauri@gmail.com>
2022-04-12 14:15:06 -0300

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://slarrauri.github.io/',
+  base: 'https://slarrauri.github.io/github-actions/',
   plugins: [react()]
 })
